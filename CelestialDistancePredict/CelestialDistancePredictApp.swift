0e03e67bc60c0581775fe693bc4c83de16bf154c
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-06-04 00:36:19 +0700

    Initial Commit

---
+++ CelestialDistancePredict/CelestialDistancePredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelestialDistancePredictApp.swift
+//  CelestialDistancePredict
+//
+//  Created by Steven Wijaya on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CelestialDistancePredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
